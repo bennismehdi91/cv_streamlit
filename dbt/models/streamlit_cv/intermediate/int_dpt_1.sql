8464940379871ca875c0c1d13f01efb62d2b391b
Mehdi Bennis <bennismehdi91@gmail.com>
2025-02-19 11:12:14 +0000

    Add intermediate tables

--- dbt/models/streamlit_cv/intermediate/int_dpt_1.sql
+++ dbt/models/streamlit_cv/intermediate/int_dpt_1.sql
@@ -3,10 +3,10 @@ WITH
         SELECT * FROM {{ ref('stg_streamlit_cv__transactions_raw') }}
     )
 SELECT
-    FORMAT_DATE('%Y-%m', date_transaction) AS date_year_month,
+    CAST(departement AS int) AS departement_int,
+    FORMAT_DATE('%Y', date_transaction) AS date_year,
     COUNT(id_transaction) as count_transaction,
     CAST(ROUND(AVG(prix),0) AS int) as avg_price,
     CAST(ROUND(AVG(SAFE_DIVIDE(prix, surface_habitable)),0) AS int) as avg_price_squaredmetters
 FROM transactions
-GROUP BY date_year_month
-ORDER BY date_year_month
+GROUP BY departement, date_year
