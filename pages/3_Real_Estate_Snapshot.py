fa38e94d548424df0292ea568ff8de3b7cd2a301
bennismehdi91 <bennismehdi91@gmail.com>
2025-03-03 14:51:57 +0100

    add session state for local accross pages

--- pages/3_Real_Estate_Snapshot.py
+++ pages/3_Real_Estate_Snapshot.py
@@ -15,8 +15,21 @@ from google.oauth2 import service_account
 FR = "Français"
 EN = "English"
 
+if "local" not in st.session_state:
+    st.session_state.local = FR  # Default local
+
+# Sidebar Local Selection
 with st.sidebar.expander("**Language**"):
-    local = st.radio("Select Language / Sélectionnez la langue:", (FR, EN))
+    local = st.radio(
+        "Select language / Sélectionnez la langue:",
+        (FR, EN),
+        index=0 if st.session_state.local == FR else 1,
+    )
+
+# Update session state when a new local is selected
+if local != st.session_state.local:
+    st.session_state.local = local
+
 
 st.write("# Real Estate Snapshot - Reverse ETL")
 
